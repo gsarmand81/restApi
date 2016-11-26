42da400b4c9e72395623e23f24947f57401e3dfe
Armando Garcia <gsarmand81@gmail.com>
2016-11-26 10:46:30 -0600

    ADD Insert Queries

--- DB/queries.sql
+++ DB/queries.sql
@@ -10,13 +10,28 @@
 SELECT * FROM rules;
 
 -- Actualizar init_hour and end_hour
-UPDATE `IOTDB`.`rules` SET `init_hour`='02:00',`end_hour`='23:59' WHERE `rule_id` in ('2','3','4','5','6','8');
+UPDATE `IOTDB`.`rules` SET `init_hour`='09:00',`end_hour`='19:00' WHERE `rule_id` in ('2','3','4','5','6','8');
 
 -- Activar Regla
-UPDATE `IOTDB`.`rules` SET `enable`=false WHERE `rule_id` in ('2','3');
+UPDATE `IOTDB`.`rules` SET `enable`=true WHERE `rule_id` in ('2','3');
 
 SELECT * from sensor_events where sensor_id = 3 order by timestamp desc limit 1 ;
 
 INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
-VALUES (true, 'Mantiene temperatura por medio del termostato', '<', '1', '4', '1', '40', '00:10', '05:00');
+VALUES (true, 'Por Presencia se prende la luz', '=', '0', '5', '1', '1', '05:00', '0:10');
+INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
+VALUES (true, 'Por Presencia se apaga la luz', '=', '0', '5', '0', '0', '05:00', '0:10');
+INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
+VALUES (true, 'Por Peso se prende el termostato', '>', '3', '4', '1', '500', '05:00', '0:10');
+INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
+VALUES (true, 'Por Peso se apage el termostato', '<', '3', '4', '0', '500', '05:00', '0:10');
+INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
+VALUES (true, 'Prende termostato si la temperatura es mayo a  25', '>', '1', '4', '1', '25', '05:00', '0:10');
+INSERT INTO `IOTDB`.`rules` (`enable`, `name`, `operator`, `param_id`, `param_id_action`, `value_action`, `value_compare`, `end_hour`, `init_hour`)  
+VALUES (true, 'Apaga termostato si la temperatura es mayo a  25', '<', '1', '4', '0', '25', '05:00', '0:10');
+
+UPDATE `IOTDB`.`rules` SET `enable`='1' WHERE `rule_id` in ('1','2','3','4','5','6');
+
+UPDATE `IOTDB`.`rules` SET `init_hour`='02:00',`end_hour`='23:59' WHERE `rule_id` in ('1','2','3','4','5','6');
+
 
